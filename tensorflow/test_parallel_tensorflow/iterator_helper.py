97b1f6ecd8b76adce7bbac80dddc425a10bde023
lcy-seso <lcy.seso@gmail.com>
2018-05-08 14:30:32 +0800

    init proj.

---
+++ tensorflow/test_parallel_tensorflow/iterator_helper.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+#coding=utf-8
+import collections
+
+import tensorflow as tf
+
+__all__ = [
+    "get_iterator",
+]
+
+
+class BatchedInput(
+        collections.namedtuple("BatchedInput", (
+            "initializer", "source", "target_input", "target_output",
+            "source_sequence_length", "target_sequence_length", ))):
+    pass
+
+
+def get_iterator(src_file_name,
+                 tgt_file_name,
+                 src_vocab_file,
+                 tgt_vocab_file,
+                 batch_size,
+                 bos="<s>",
+                 eos="</s>",
+                 unk_id=0,
+                 src_max_len=None,
+                 tgt_max_len=None,
+                 num_parallel_calls=4,
+                 num_buckets=5,
+                 output_buffer_size=None,
+                 disable_shuffle=False):
+    def __get_word_dict(vocab_file_path, unk_id):
+        return tf.contrib.lookup.index_table_from_file(
+            vocabulary_file=vocab_file_path,
+            key_column_index=0,
+            default_value=unk_id)
+
+    if not output_buffer_size:
+        output_buffer_size = batch_size * 1000
+
+    src_vocab_table = __get_word_dict(src_vocab_file, unk_id)
+    tgt_vocab_table = __get_word_dict(tgt_vocab_file, unk_id)
+
+    src_eos_id = tf.cast(src_vocab_table.lookup(tf.constant(eos)), tf.int32)
+    tgt_bos_id = tf.cast(tgt_vocab_table.lookup(tf.constant(bos)), tf.int32)
+    tgt_eos_id = tf.cast(tgt_vocab_table.lookup(tf.constant(eos)), tf.int32)
+
+    src_dataset = tf.data.TextLineDataset(src_file_name)
+    tgt_dataset = tf.data.TextLineDataset(tgt_file_name)
+
+    dataset = tf.data.Dataset.zip((src_dataset, tgt_dataset))
+    if not disable_shuffle:
+        dataset = dataset.shuffle(
+            buffer_size=output_buffer_size, reshuffle_each_iteration=True)
+
+    src_tgt_dataset = dataset.map(
+        lambda src, tgt: (tf.string_split([src]).values, \
+                tf.string_split([tgt]).values),
+        num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+
+    # Filter zero length input sequences.
+    src_tgt_dataset = src_tgt_dataset.filter(
+        lambda src, tgt: tf.logical_and(tf.size(src) > 0, tf.size(tgt) > 0))
+
+    if src_max_len:
+        src_tgt_dataset = src_tgt_dataset.map(
+            lambda src, tgt: (src[:src_max_len], tgt),
+            num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+    if tgt_max_len:
+        src_tgt_dataset = src_tgt_dataset.map(
+            lambda src, tgt: (src, tgt[:tgt_max_len]),
+            num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+
+    # convert word string to word index
+    src_tgt_dataset = src_tgt_dataset.map(
+        lambda src, tgt: (
+               tf.cast(src_vocab_table.lookup(src), tf.int32),
+                tf.cast(tgt_vocab_table.lookup(tgt), tf.int32)),
+        num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+
+    src_tgt_dataset = src_tgt_dataset.map(
+        lambda src, tgt: (src,
+                          tf.concat(([tgt_bos_id], tgt), 0),
+                          tf.concat((tgt, [tgt_eos_id]), 0)),
+        num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+
+    # Add in sequence lengths.
+    src_tgt_dataset = src_tgt_dataset.map(
+        lambda src, tgt_in, tgt_out: (\
+                src, tgt_in, tgt_out, tf.size(src), tf.size(tgt_in)),
+        num_parallel_calls=num_parallel_calls).prefetch(output_buffer_size)
+
+    def __batching_func(x):
+        return x.padded_batch(
+            batch_size,
+            padded_shapes=(
+                tf.TensorShape([None]),  # src
+                tf.TensorShape([None]),  # tgt_in
+                tf.TensorShape([None]),  # tgt_out
+                tf.TensorShape([]),  # src_len
+                tf.TensorShape([]),  # tgt_len
+            ),
+            padding_values=(src_eos_id, tgt_eos_id, tgt_eos_id, 0, 0))
+
+    if num_buckets > 1:
+
+        def __key_func(unused_1, unused_2, unused_3, src_len, tgt_len):
+            if src_max_len:
+                bucket_width = (src_max_len + num_buckets - 1) // num_buckets
+            else:
+                bucket_width = 10
+
+            bucket_id = tf.maximum(src_len // bucket_width,
+                                   tgt_len // bucket_width)
+            return tf.to_int64(tf.minimum(num_buckets, bucket_id))
+
+        def __reduce_func(unused_key, windowed_data):
+            return __batching_func(windowed_data)
+
+        batched_dataset = src_tgt_dataset.apply(
+            tf.contrib.data.group_by_window(
+                key_func=__key_func,
+                reduce_func=__reduce_func,
+                window_size=batch_size))
+
+    else:
+        batched_dataset = __batching_func(curwd_nxtwd_dataset)
+
+    batched_iter = batched_dataset.make_initializable_iterator()
+    (src_ids, tgt_input_ids, tgt_output_ids, src_seq_len,
+     tgt_seq_len) = batched_iter.get_next()
+    return BatchedInput(
+        initializer=batched_iter.initializer,
+        source=src_ids,
+        target_input=tgt_input_ids,
+        target_output=tgt_output_ids,
+        source_sequence_length=src_seq_len,
+        target_sequence_length=tgt_seq_len)
+
+
+if __name__ == "__main__":
+    src_file_name = "data/train.en"
+    tgt_file_name = "data/train.de"
+    src_vocab_file = "data/vocab.50K.en"
+    tgt_vocab_file = "data/vocab.50K.de"
+
+    batch_size = 2
+
+    iterator = get_iterator(src_file_name, tgt_file_name, src_vocab_file,
+                            tgt_vocab_file, batch_size)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.tables_initializer())
+        sess.run(iterator.initializer)
+
+        for i in range(5):
+            src_ids, tgt_input_ids, tgt_output_ids, src_len, tgt_len = sess.run(
+                [
+                    iterator.source,
+                    iterator.target_input,
+                    iterator.target_output,
+                    iterator.source_sequence_length,
+                    iterator.target_sequence_length,
+                ])
+            print src_ids, tgt_input_ids, tgt_output_ids
---
+++ tensorflow/test_parallel_tensorflow/model.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+#coding=utf-8
+import pdb
+import os
+
+import tensorflow as tf
+from tensorflow.contrib.seq2seq import *
+from tensorflow.python.layers.core import Dense
+
+from iterator_helper import get_iterator
+
+hparams = tf.contrib.training.HParams(
+    src_file_name="data/train.en",
+    tgt_file_name="data/train.de",
+    src_vocab_file="data/vocab.50K.en",
+    tgt_vocab_file="data/vocab.50K.de",
+    src_vocab_size=50000,
+    tgt_vocab_size=50000,
+    bos="<s>",
+    eos="</s>",
+    unk_id=0,
+    src_max_len=None,
+    tgt_max_len=None,
+    num_parallel_calls=4,
+    num_buckets=5,
+    output_buffer_size=None,
+    disable_shuffle=False,
+    batch_size=16,
+
+    # hyper parameters for model topology
+    time_major=False,
+    dropout=0.,
+    unit_type="lstm",
+    num_units=512,
+    forget_bias=1.,
+    embedding_dim=512,
+    encoder_type="bi",
+    num_encoder_layers=2,
+    num_decoder_layers=2,
+    optimizer="adam",
+    learning_rate=0.001,
+    num_keep_ckpts=5,
+    max_gradient_norm=5., )
+
+
+class Seq2SeqModel(object):
+    def __init__(self, iterator, hparams,
+                 mode=tf.contrib.learn.ModeKeys.TRAIN):
+        self.iterator = iterator
+        self.mode = mode
+
+        self.source = iterator.source
+        self.target_input = iterator.target_input
+        self.target_output = iterator.target_output
+        self.source_sequence_length = iterator.source_sequence_length
+        self.target_sequence_length = iterator.target_sequence_length
+
+        self.batch_size = tf.size(self.source_sequence_length)
+
+        self.logits, self.loss, self.final_context_state = self.build_graph(
+            self.source, self.target_input, self.target_output,
+            self.source_sequence_length, self.target_sequence_length, hparams)
+
+        if self.mode == tf.contrib.learn.ModeKeys.TRAIN:
+            self.train_loss = self.loss / tf.to_float(self.batch_size)
+            self.word_count = tf.reduce_sum(
+                self.iterator.source_sequence_length) + tf.reduce_sum(
+                    self.iterator.target_sequence_length)
+
+            self.learning_rate = tf.constant(hparams.learning_rate)
+            self.global_step = tf.Variable(0, trainable=False)
+
+            # Optimizer
+            if hparams.optimizer == "sgd":
+                opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+            elif hparams.optimizer == "adam":
+                opt = tf.train.AdamOptimizer(self.learning_rate)
+
+            colocate_gradients_with_ops = True
+            params = tf.trainable_variables()
+            gradients = tf.gradients(
+                self.train_loss,
+                params,
+                colocate_gradients_with_ops=colocate_gradients_with_ops, )
+
+            clipped_gradients, gradient_norm = tf.clip_by_global_norm(
+                gradients, hparams.max_gradient_norm)
+            self.grad_norm = gradient_norm
+
+            self.update = opt.apply_gradients(
+                zip(clipped_gradients, params), global_step=self.global_step)
+
+        elif self.mode == tf.contrib.learn.ModeKeys.EVAL:
+            #TODO(caoying): not implemented yet.
+            raise NotImplementedError("To be implemented")
+        elif self.mode == tf.contrib.learn.ModeKeys.INFER:
+            #TODO(caoying): not implemented yet.
+            raise NotImplementedError("To be implemented")
+
+        self.saver = tf.train.Saver(
+            tf.global_variables(), max_to_keep=hparams.num_keep_ckpts)
+
+    def build_graph(self,
+                    source,
+                    target_input,
+                    target_output,
+                    source_sequence_length,
+                    target_sequence_length,
+                    hparams,
+                    dtype=tf.float32,
+                    scope=None):
+
+        source_embed = self._init_embeddings("src_embedding",
+                                             hparams.embedding_dim,
+                                             hparams.src_vocab_size, dtype)
+        target_embed = self._init_embeddings("tgt_embedding",
+                                             hparams.embedding_dim,
+                                             hparams.tgt_vocab_size, dtype)
+
+        with tf.variable_scope("decoder/output_projection"):
+            self.output_layer = Dense(
+                hparams.tgt_vocab_size,
+                use_bias=False,
+                name="output_projection")
+
+        with tf.variable_scope(scope or "dynamic_seq2seq", dtype=dtype):
+            encoder_outputs, encoder_state = self._build_encoder(
+                hparams, source, source_embed, source_sequence_length, dtype)
+
+            logits, final_context_state = self._build_decoder(
+                hparams, encoder_outputs, encoder_state,
+                source_sequence_length, target_embed, target_input,
+                target_sequence_length)
+
+            if self.mode != tf.contrib.learn.ModeKeys.INFER:
+                loss = self._compute_loss(logits, target_output,
+                                          target_sequence_length,
+                                          hparams.time_major)
+            else:
+                loss = None
+
+            return logits, loss, final_context_state
+
+    def _init_embeddings(self, embed_name, embedding_dim, vocab_size, dtype):
+        return tf.get_variable(
+            embed_name, [vocab_size, embedding_dim], dtype=dtype)
+
+    def _single_cell(self, unit_type, num_units, forget_bias, dropout, mode):
+        dropout = dropout if mode == tf.contrib.learn.ModeKeys.TRAIN else 0.0
+
+        # Cell Type
+        if unit_type == "lstm":
+            single_cell = tf.contrib.rnn.BasicLSTMCell(
+                num_units, forget_bias=forget_bias)
+        elif unit_type == "gru":
+            single_cell = tf.contrib.rnn.GRUCell(num_units)
+        elif unit_type == "layer_norm_lstm":
+            single_cell = tf.contrib.rnn.LayerNormBasicLSTMCell(
+                num_units, forget_bias=forget_bias, layer_norm=True)
+        elif unit_type == "nas":
+            single_cell = tf.contrib.rnn.NASCell(num_units)
+        else:
+            raise ValueError("Unknown unit type %s!" % unit_type)
+
+        # Dropout (= 1 - keep_prob)
+        if dropout > 0.0:
+            single_cell = tf.contrib.rnn.DropoutWrapper(
+                cell=single_cell, input_keep_prob=(1.0 - dropout))
+
+        return single_cell
+
+    def _build_rnn_cell(self, num_layers, unit_type, num_units, forget_bias,
+                        dropout, mode):
+        cell_list = []
+        for i in range(num_layers):
+            cell_list.append(
+                self._single_cell(
+                    unit_type=unit_type,
+                    num_units=num_units,
+                    forget_bias=forget_bias,
+                    dropout=dropout,
+                    mode=mode, ))
+        if len(cell_list) == 1:  # Single layer.
+            return cell_list[0]
+        else:  # Multi layers
+            return tf.contrib.rnn.MultiRNNCell(cell_list)
+
+    def _build_bidirectional_rnn(self, inputs, num_layers, unit_type,
+                                 num_units, forget_bias, dropout, mode,
+                                 sequence_length, dtype, time_major):
+        # Construct forward and backward cells
+        fw_cell = self._build_rnn_cell(num_layers, unit_type, num_units,
+                                       forget_bias, dropout, mode)
+        bw_cell = self._build_rnn_cell(num_layers, unit_type, num_units,
+                                       forget_bias, dropout, mode)
+
+        bi_outputs, bi_state = tf.nn.bidirectional_dynamic_rnn(
+            fw_cell,
+            bw_cell,
+            inputs,
+            dtype=dtype,
+            sequence_length=sequence_length,
+            time_major=time_major,
+            swap_memory=True)
+
+        return tf.concat(bi_outputs, -1), bi_state
+
+    def _build_encoder(self, hparams, source, source_embed,
+                       source_sequence_length, dtype):
+        num_layers = hparams.num_encoder_layers
+
+        if hparams.time_major:
+            source = tf.transpose(source)
+
+        with tf.variable_scope("encoder") as scope:
+            encoder_emb_inp = tf.nn.embedding_lookup(source_embed, source)
+
+            # Encoder_outputs: [max_time, batch_size, num_units]
+            if hparams.encoder_type == "uni":
+                cell = self._build_rnn_cell(
+                    num_layers=hparams.num_encoder_layers,
+                    unit_type=hparams.unit_type,
+                    num_units=hparams.num_units,
+                    forget_bias=hparams.forget_bias,
+                    dropout=hparams.dropout,
+                    mode=self.mode)
+
+                encoder_outputs, encoder_state = tf.nn.dynamic_rnn(
+                    cell,
+                    encoder_emb_inp,
+                    dtype=dtype,
+                    sequence_length=source_sequence_length,
+                    time_major=hparams.time_major,
+                    swap_memory=True)
+
+            elif hparams.encoder_type == "bi":
+                num_bi_layers = int(num_layers / 2)
+
+                encoder_outputs, bi_encoder_state = (
+                    self._build_bidirectional_rnn(
+                        inputs=encoder_emb_inp,
+                        num_layers=num_bi_layers,
+                        unit_type=hparams.unit_type,
+                        num_units=hparams.num_units,
+                        forget_bias=hparams.forget_bias,
+                        dropout=hparams.dropout,
+                        mode=self.mode,
+                        sequence_length=source_sequence_length,
+                        dtype=dtype,
+                        time_major=hparams.time_major))
+
+                if num_bi_layers == 1:
+                    encoder_state = bi_encoder_state
+                else:
+                    # alternatively concat forward and backward states
+                    encoder_state = []
+                    for layer_id in range(num_bi_layers):
+                        encoder_state.append(
+                            bi_encoder_state[0][layer_id])  # forward
+                        encoder_state.append(
+                            bi_encoder_state[1][layer_id])  # backward
+                    encoder_state = tuple(encoder_state)
+            else:
+                raise ValueError("Unknown encoder_type %s" %
+                                 hparams.encoder_type)
+        return encoder_outputs, encoder_state
+
+    def _build_decoder_cell(self, hparams, encoder_outputs, encoder_state):
+        cell = self._build_rnn_cell(
+            num_layers=hparams.num_decoder_layers,
+            unit_type=hparams.unit_type,
+            num_units=hparams.num_units,
+            forget_bias=hparams.forget_bias,
+            dropout=hparams.dropout,
+            mode=self.mode)
+
+        if (self.mode == tf.contrib.learn.ModeKeys.INFER and
+                hparams.beam_width > 0):
+            #TODO(caoying): not implemented yet.
+            raise NotImplementedError("To be implemented")
+        else:
+            decoder_initial_state = encoder_state
+
+        return cell, decoder_initial_state
+
+    def _build_decoder(self, hparams, encoder_outputs, encoder_state,
+                       source_sequence_length, target_embed, target_input,
+                       target_sequence_length):
+
+        with tf.variable_scope("decoder") as decoder_scope:
+            cell, decoder_initial_state = self._build_decoder_cell(
+                hparams, encoder_outputs, encoder_state)
+
+            if self.mode != tf.contrib.learn.ModeKeys.INFER:
+                if hparams.time_major:
+                    target_input = tf.transpose(target_input)
+
+                decoder_emb_inp = tf.nn.embedding_lookup(target_embed,
+                                                         target_input)
+
+                # Helper
+                helper = tf.contrib.seq2seq.TrainingHelper(
+                    decoder_emb_inp,
+                    target_sequence_length,
+                    time_major=hparams.time_major)
+
+                # Decoder
+                my_decoder = tf.contrib.seq2seq.BasicDecoder(
+                    cell,
+                    helper,
+                    decoder_initial_state, )
+
+                # Dynamic decoding
+                (outputs, final_context_state,
+                 _) = tf.contrib.seq2seq.dynamic_decode(
+                     my_decoder,
+                     output_time_major=hparams.time_major,
+                     swap_memory=True,
+                     scope=decoder_scope)
+                logits = self.output_layer(outputs.rnn_output)
+            else:
+                #TODO(caoying): not implemented yet.
+                raise NotImplementedError("To be implemented")
+
+        return logits, final_context_state
+
+    def _get_max_time(self, tensor, time_major):
+        time_axis = 0 if time_major else 1
+        return tensor.shape[time_axis].value or tf.shape(tensor)[time_axis]
+
+    def _compute_loss(self, logits, target_output, target_sequence_length,
+                      time_major):
+        if time_major:
+            target_output = tf.transpose(target_output)
+
+        max_time = self._get_max_time(target_output, time_major)
+
+        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
+            labels=target_output, logits=logits)
+        target_weights = tf.sequence_mask(
+            target_sequence_length, max_time, dtype=logits.dtype)
+        if time_major:
+            target_weights = tf.transpose(target_weights)
+        return tf.reduce_sum(crossent * target_weights)
+
+
+if __name__ == "__main__":
+    iterator = get_iterator(hparams.src_file_name, hparams.tgt_file_name,
+                            hparams.src_vocab_file, hparams.tgt_vocab_file,
+                            hparams.batch_size)
+    model = Seq2SeqModel(iterator, hparams)
