142ee36d8cfd75904e020802a0aefba7663fb43b
lcy-seso <lcy.seso@gmail.com>
2018-05-31 18:33:03 +0800

    merge gradient by using all-reduce. init.

--- tensorflow/data_parallelism_for_nmt/seq2seq_model.py
+++ tensorflow/data_parallelism_for_nmt/seq2seq_model.py
@@ -56,8 +56,17 @@ hparams = tf.contrib.training.HParams(
     max_gradient_norm=5.,
 
     # parameter server places
+    variable_update="replicated",
     param_server_device="cpu",
-    local_parameter_device="gpu", )
+    local_parameter_device="gpu",
+
+    # used for all reduced algorithm
+    num_gpus=2,
+    variable_consistency="relaxed",
+    gradient_repacking=4,
+    all_reduce_spec="nccl",
+    agg_small_grads_max_bytes=0,
+    agg_small_grads_max_group=10, )
 
 
 class Seq2SeqModel(object):
@@ -65,8 +74,8 @@ class Seq2SeqModel(object):
                  num_gpus,
                  hparams,
                  mode=tf.contrib.learn.ModeKeys.TRAIN,
-                 worker_prefix=''):
-
+                 worker_prefix=""):
+        self.params = hparams
         self.num_gpus = num_gpus
         # NOTE: batch size passed to get_iterator here is batch size for a single
         # GPU card. the total batch size is num_splits *  hparams.batch_size
@@ -97,23 +106,34 @@ class Seq2SeqModel(object):
         # trainable parameters
         self.param_server_device = hparams.param_server_device
         self.local_parameter_device = hparams.local_parameter_device
-        # Device to use for ops that need to always run on the local worker's CPU.
-        self.cpu_device = '%s/cpu:0' % worker_prefix
+        # Device to use for ops that need to always run on the local worker"s CPU.
+        self.cpu_device = "%s/cpu:0" % worker_prefix
         # devices for computation workers
         self.raw_devices = [
-            '%s/%s:%i' % (worker_prefix, hparams.local_parameter_device, i)
+            "%s/%s:%i" % (worker_prefix, hparams.local_parameter_device, i)
             for i in xrange(self.num_gpus)
         ]
 
-        self.variable_mgr = variable_mgr.VariableMgrLocalFetchFromPS(self)
-        # Device to use for running on the local worker's compute device, but
+        if hparams.variable_update == "replicated":
+            self.variable_mgr = variable_mgr.VariableMgrLocalReplicated(
+                self, self.params.all_reduce_spec,
+                self.params.agg_small_grads_max_bytes,
+                self.params.agg_small_grads_max_group)
+        elif hparams.variable_update == "parameter_server":
+            self.variable_mgr = variable_mgr.VariableMgrLocalFetchFromPS(self)
+        else:
+            raise ValueError(
+                ("Unsupported setting for variable_update. "
+                 "Possible setting is parameter_server and replicated."))
+
+        # Device to use for running on the local worker"s compute device, but
         # with variables assigned to parameter server devices.
         self.devices = self.variable_mgr.get_devices()
 
         self.global_step_device = self.cpu_device
 
-        self.fetches = self._make_parallel(
-            self.build_graph,
+        self.fetches = self.make_data_parallel(
+            self.build_mode_replica,
             hparams=hparams,
             source=self.source,
             target_input=self.target_input,
@@ -136,24 +156,27 @@ class Seq2SeqModel(object):
         self.saver = tf.train.Saver(
             tf.global_variables(), max_to_keep=hparams.num_keep_ckpts)
 
-    def _make_parallel(self, fn, **kwargs):
+    def make_data_parallel(self, fn, **kwargs):
         """ Wrapper for data parallelism.
         """
 
         in_splits = {}
         for k, v in kwargs.items():
-            if isinstance(v, list):
+            if isinstance(v, list):  # input tensors
                 in_splits[k] = v
-            else:
+            else:  # hyper parameters
                 in_splits[k] = [v] * len(self.devices)
 
         losses = []
         device_grads = []
         for device_num in range(len(self.devices)):
+            # when using PS mode, learnable parameters are placed on different
+            # GPU devices. when using all-reduced algorithm, each GPU card has
+            # a entire copy of model parameters.
             with self.variable_mgr.create_outer_variable_scope(
-                    device_num), tf.name_scope('tower_%i' %
+                    device_num), tf.name_scope("tower_%i" %
                                                device_num) as name_scope:
-                results = self._add_forward_pass_and_gradients(
+                results = self.add_forward_pass_and_gradients(
                     device_num, device_num,
                     **{k: v[device_num]
                        for k, v in in_splits.items()})
@@ -163,10 +186,11 @@ class Seq2SeqModel(object):
         with tf.device(self.global_step_device):
             self.global_step = tf.train.get_or_create_global_step()
 
-        return self._build_fetches(self.global_step, losses, device_grads)
+        return self.build_gradient_merge_and_update(self.global_step, losses,
+                                                    device_grads)
 
-    def _add_forward_pass_and_gradients(self, rel_device_num, abs_device_num,
-                                        **inputs):
+    def add_forward_pass_and_gradients(self, rel_device_num, abs_device_num,
+                                       **inputs):
         """
         Args:
           rel_device_num: local worker device index.
@@ -174,7 +198,7 @@ class Seq2SeqModel(object):
         """
 
         with tf.device(self.devices[rel_device_num]):
-            logits, loss, final_context_state = self.build_graph(
+            logits, loss, final_context_state = self.build_mode_replica(
                 source=inputs["source"],
                 target_input=inputs["target_input"],
                 target_output=inputs["target_output"],
@@ -193,7 +217,8 @@ class Seq2SeqModel(object):
                 "gradvars": gradvars,
             }
 
-    def _build_fetches(self, global_step, losses, device_grads):
+    def build_gradient_merge_and_update(self, global_step, losses,
+                                        device_grads):
         fetches = {}
 
         apply_gradient_devices = self.devices
@@ -201,41 +226,45 @@ class Seq2SeqModel(object):
 
         training_ops = []
         average_loss = tf.reduce_mean(losses)
-        avg_grads = self.variable_mgr.get_gradients_to_apply(gradient_state)
 
-        self.learning_rate = tf.constant(hparams.learning_rate)
+        # merget gradients using PS mode or all-reduce algorithm
+        apply_gradient_devices, gradient_state = (
+            self.variable_mgr.preprocess_device_grads(device_grads))
 
-        #TODO(caoying): add gradient clipping.
-        opt = tf.train.AdamOptimizer(self.learning_rate)
+        for d, device in enumerate(apply_gradient_devices):
+            avg_grads = self.variable_mgr.get_gradients_to_apply(
+                gradient_state)
 
-        loss_scale_params = variable_mgr_util.AutoLossScaleParams(
-            enable_auto_loss_scale=False,
-            loss_scale=None,
-            loss_scale_normal_steps=None,
-            inc_loss_scale_every_n=1000,
-            is_chief=True)
+            self.learning_rate = tf.constant(hparams.learning_rate)
 
-        # append optimizer operators into the graph
-        with tf.device(self.raw_devices[0]):
-            self.variable_mgr.append_apply_gradients_ops(
-                gradient_state,
-                opt,
-                avg_grads,
-                training_ops,
-                loss_scale_params, )
+            #TODO(caoying): add gradient clipping.
+            opt = tf.train.AdamOptimizer(self.learning_rate)
 
-        fetches['train_op'] = tf.group(training_ops)
-        fetches['average_loss'] = average_loss / tf.to_float(self.batch_size)
+            loss_scale_params = variable_mgr_util.AutoLossScaleParams(
+                enable_auto_loss_scale=False,
+                loss_scale=None,
+                loss_scale_normal_steps=None,
+                inc_loss_scale_every_n=1000,
+                is_chief=True)
+
+            # append optimizer operators into the graph
+            with tf.device(self.raw_devices[0]):
+                self.variable_mgr.append_apply_gradients_ops(
+                    gradient_state, opt, avg_grads, training_ops,
+                    loss_scale_params)
+
+        fetches["train_op"] = tf.group(training_ops)
+        fetches["average_loss"] = average_loss / tf.to_float(self.batch_size)
         return fetches
 
-    def build_graph(self,
-                    source,
-                    target_input,
-                    target_output,
-                    source_sequence_length,
-                    target_sequence_length,
-                    hparams,
-                    dtype=tf.float32):
+    def build_mode_replica(self,
+                           source,
+                           target_input,
+                           target_output,
+                           source_sequence_length,
+                           target_sequence_length,
+                           hparams,
+                           dtype=tf.float32):
 
         self.output_layer = Dense(
             hparams.tgt_vocab_size, use_bias=False, name="output_projection")
@@ -256,14 +285,8 @@ class Seq2SeqModel(object):
 
         return logits, loss, final_context_state
 
-    def _init_embeddings(self,
-                         input,
-                         embed_name,
-                         embedding_dim,
-                         vocab_size,
-                         dtype,
-                         device="/cpu:0"):
-        # with tf.device(device):
+    def _init_embeddings(self, input, embed_name, embedding_dim, vocab_size,
+                         dtype):
         embed_var = tf.get_variable(
             embed_name, [vocab_size, embedding_dim], dtype=dtype)
         return tf.nn.embedding_lookup(embed_var, input)
--- tensorflow/data_parallelism_for_nmt/variable_mgr.py
+++ tensorflow/data_parallelism_for_nmt/variable_mgr.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 import pdb
 import tensorflow as tf
 
+import allreduce
+import batch_allreduce
 import variable_mgr_util
 
 
@@ -160,3 +162,98 @@ class VariableMgrLocalFetchFromPS(VariableMgr):
                     ps_device=self.model_helper.param_server_device,
                     ps_tasks=1) for d in raw_devices
             ]
+
+
+class VariableMgrLocalReplicated(VariableMgr):
+    """VariableMgr that implements the --replicated mode for local jobs.
+
+    Each GPU has its own copy of the variables. To apply gradients,
+    either a local all-reduce algorithm is applied or a regular
+    cross-device aggregation is used to replicate the combined
+    gradients to all towers.
+    """
+
+    def __init__(self, model_helper, all_reduce_spec,
+                 agg_small_grads_max_bytes, agg_small_grads_max_group):
+        super(VariableMgrLocalReplicated, self).__init__(model_helper)
+        if all_reduce_spec:
+            spec = allreduce.parse_all_reduce_spec(all_reduce_spec)
+            if len(spec) != 1:
+                raise ValueError(("replicated mode does not support "
+                                  "hybrid all-reduce strategies"))
+            self._all_reduce_spec = spec[0]
+        else:
+            self._all_reduce_spec = None
+        self._agg_small_grads_max_bytes = agg_small_grads_max_bytes
+        self._agg_small_grads_max_group = agg_small_grads_max_group
+        self._warmup_ops = []
+        self._gradient_put_ops = None
+
+    def each_tower_has_variables(self):
+        return True
+
+    def create_outer_variable_scope(self, device_num):
+        return tf.variable_scope(
+            "v%s" % device_num, use_resource=self.use_resource_vars)
+
+    def preprocess_device_grads(self, device_grads):
+        compact_grads = False
+        defer_grads = (
+            self.model_helper.params.variable_consistency == "relaxed")
+
+        grads_to_reduce = [[g for g, _ in grad_vars]
+                           for grad_vars in device_grads]
+
+        algorithm = batch_allreduce.algorithm_from_params(
+            self.model_helper.params)
+        reduced_grads, self._warmup_ops = algorithm.batch_all_reduce(
+            grads_to_reduce, self.model_helper.params.gradient_repacking,
+            compact_grads, defer_grads)
+
+        if self.model_helper.enable_auto_loss_scale:
+            # Check for infs or nans
+            is_finite_list = []
+            for tower_grads in reduced_grads:
+                with tf.colocate_with(tower_grads[0]):
+                    is_finite_list.append(
+                        tf.reduce_all([
+                            tf.reduce_all(tf.is_finite(g)) for g in tower_grads
+                        ]))
+            self.grad_has_inf_nan = tf.logical_not(
+                tf.reduce_all(is_finite_list))
+        reduced_device_grads = [[
+            (g, v) for g, (_, v) in zip(grads, grad_vars)
+        ] for grads, grad_vars in zip(reduced_grads, device_grads)]
+
+        return self.model_helper.devices, reduced_device_grads
+
+    def get_gradients_to_apply(self, device_num, gradient_state):
+        device_grads = gradient_state
+        return device_grads[device_num]
+
+    def get_post_init_ops(self):
+        # Copy initialized values for variables on GPU 0 to other GPUs.
+        global_vars = tf.global_variables()
+        var_by_name = dict([(v.name, v) for v in global_vars])
+        post_init_ops = []
+        for v in global_vars:
+            split_name = v.name.split("/")
+            if split_name[0] == "v0" or not v.name.startswith("v"):
+                continue
+            split_name[0] = "v0"
+            copy_from = var_by_name["/".join(split_name)]
+            post_init_ops.append(v.assign(copy_from.read_value()))
+        post_init_ops += self._warmup_ops
+        return post_init_ops
+
+    def savable_variables(self):
+        """Return the set of variables used for saving/loading the model."""
+        params = []
+        for v in tf.global_variables():
+            split_name = v.name.split("/")
+            if split_name[0] == "v0" or not v.name.startswith("v"):
+                params.append(v)
+        return params
+
+    def get_devices(self):
+        return self.model_helper.raw_devices
