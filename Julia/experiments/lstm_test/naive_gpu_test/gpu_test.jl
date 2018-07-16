5921b24584072902b2082f40008110060152ce96
lcy-seso <lcy.seso@gmail.com>
2018-07-16 09:29:28 +0800

    updates.

--- Julia/experiments/lstm_test/naive_gpu_test/gpu_test.jl
+++ Julia/experiments/lstm_test/naive_gpu_test/gpu_test.jl
@@ -6,18 +6,25 @@ using CuArrays: CuArray
 
 srand(1)
 
-const batch_size = 2
-const seq_len = 3
-const input_dim = 4
-const hidden_dim = 4
+const batch_size = 512
+const seq_len = 10
+const input_dim = 512
+const hidden_dim = 512
 
 rand_inputs_d = CuArray(randn(batch_size * seq_len, input_dim))
 lstm_cell = LSTMCell(input_dim, hidden_dim)
 
-cell_states, hidden_states = lstm_forward(rand_inputs_d, lstm_cell, seq_len)
 
-println("cell states : ")
-display(cell_states)
+for i = 1 : 100
+  if i % 10 == 0
+    println(i)
+  end
 
-println("hidden_states : ")
-display(hidden_states)
+  cell_states, hidden_states = lstm_forward(rand_inputs_d, lstm_cell, seq_len)
+
+  # println("cell states : ")
+  # display(cell_states)
+
+  # println("hidden_states : ")
+  # display(hidden_states)
+end
