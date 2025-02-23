91ddae98d9dcbaa823183d81429e7698b7322ef1
Cao Ying <lcy.seso@gmail.com>
2025-02-23 19:25:59 +0800

    fix: Clean up the implementations of the 'Hello World' example for the TAM load. (#42)

--- Cuda/hopper_gemm/tma_load_store/tma_load.cu
+++ Cuda/hopper_gemm/tma_load_store/tma_load.cu
@@ -4,75 +4,67 @@
 #include <thrust/device_vector.h>
 #include <thrust/host_vector.h>
 
-#include <cstdio>
-
 using namespace cute;
 
-template <class T, class TensorX, class GmemLayout, class SmemLayout,
-          class TmaLoad>
-__global__ static void tma_kernel(TensorX tX, GmemLayout gmem_layout,
-                                  SmemLayout smem_layout,
-                                  CUTE_GRID_CONSTANT const TmaLoad tma_load) {
-  __shared__ T smem[cosize_v<SmemLayout>];
-  __shared__ uint64_t tma_load_mbar[1];
+template <typename DType, typename DataLayout, typename TmaLoad,
+          const int kShmSize>
+__global__ void ke_tma_load(const DType* data,  //
+                            DataLayout g_layout,
+                            CUTE_GRID_CONSTANT const TmaLoad tma_load) {
+  __shared__ DType smem[kShmSize];
+  __shared__ uint64_t tma_load_mbar;
 
-  auto sX = make_tensor(make_smem_ptr(smem), smem_layout);
-
-  auto mX = tma_load.get_tma_tensor(shape(gmem_layout));
-  // (CTA_TILE_M,CTA_TILE_N,...REST_M,REST_N,...)
-  auto gX = local_tile(mX, shape(smem_layout), make_coord(0, 0));
-
-  auto cta_tma_load = tma_load.get_slice(0);
-
-  auto tXgX = cta_tma_load.partition_S(gX);  // (TMA,TMA_M,TMA_N,REST_M,REST_N)
-  auto tXsX = cta_tma_load.partition_D(sX);  // (TMA,TMA_M,TMA_N)
+  // in this simple example global memory and shared memory have the same layout
+  auto g_tensor = make_tensor(make_gmem_ptr(data), g_layout);
+  auto s_tensor = make_tensor(make_smem_ptr(smem), g_layout);
 
   auto warp_idx = cutlass::canonical_warp_idx_sync();
-  auto lane_predicate = cute::elect_one_sync();
-
+  auto lane_predicate = elect_one_sync();
   if (warp_idx == 0 && lane_predicate) {
-    constexpr int k_tma_transaction_bytes = size(sX) * sizeof_bits_v<T> / 8;
+    initialize_barrier(tma_load_mbar, 1 /* arrival count */);
+    set_barrier_transaction_bytes(tma_load_mbar, kShmSize * sizeof(DType));
 
-    tma_load_mbar[0] = 0;
-    cute::initialize_barrier(tma_load_mbar[0], 1 /*numThreads*/);
-    cute::set_barrier_transaction_bytes(tma_load_mbar[0],
-                                        k_tma_transaction_bytes);
+    auto g_tensor_coord = tma_load.get_tma_tensor(shape(g_layout));
+    auto g_tensor_coord_cta =
+        local_tile(g_tensor_coord, shape(g_layout),
+                   make_coord(0, 0) /*we use 1 block in this example*/);
 
-    cute::copy(tma_load.with(tma_load_mbar[0]), tXgX, tXsX);
+    auto tma_load_per_cta = tma_load.get_slice(0);
+    copy(tma_load.with(tma_load_mbar),
+         tma_load_per_cta.partition_S(g_tensor_coord_cta),
+         tma_load_per_cta.partition_D(s_tensor));
   }
   __syncthreads();
-  constexpr int k_phase_bit = 0;
-  cute::wait_barrier(tma_load_mbar[0], k_phase_bit);
+  wait_barrier(tma_load_mbar, 0);
 }
 
 int main() {
-  using T = float;
+  using DType = float;
 
-  constexpr int m = 4;
-  constexpr int n = 4;
+  constexpr int kM = 64;
+  constexpr int kN = 128;
+  constexpr int kNumel = kM * kN;
 
-  thrust::host_vector<T> h_data(m * n);
-  for (int i = 0; i < m * n; ++i) h_data[i] = static_cast<T>(i);
-  thrust::device_vector<T> d_data = h_data;
+  thrust::host_vector<DType> h_data(kNumel);
+  for (int i = 0; i < kNumel; ++i) {
+    h_data[i] = static_cast<DType>(i);
+  }
+  thrust::device_vector<DType> d_data = h_data;
   cudaDeviceSynchronize();
+  const DType* d_data_ptr = d_data.data().get();
 
-  // create tensors
-  auto gmem_layout = Layout<Shape<Int<n>, Int<m>>>{};
-  auto smem_layout = Layout<Shape<Int<n>, Int<m>>>{};
+  using DataLayout = Layout<Shape<Int<kM>, Int<kN>>, Stride<Int<kN>, _1>>;
+  DataLayout layout;
 
-  auto gX = make_tensor(
-      make_gmem_ptr(reinterpret_cast<const T*>(d_data.data().get())),
-      gmem_layout);
+  auto g_tensor = make_tensor(
+      make_gmem_ptr(reinterpret_cast<const DType*>(d_data_ptr)), layout);
+  auto tma_load = make_tma_copy(SM90_TMA_LOAD{}, g_tensor, layout);
 
-  // create the TMA object
-  auto tma_load = make_tma_copy(SM90_TMA_LOAD{}, gX, smem_layout);
+  dim3 blocks(1, 1, 1);
+  dim3 threads(1, 1, 1);
+  auto kernel = &ke_tma_load<DType, DataLayout, decltype(tma_load), kM * kN>;
 
-  // launch the kernel
-  dim3 blk_dim{1, 1, 1};
-  dim3 grd_dim{1, 1, 1};
-  tma_kernel<T, decltype(gX), decltype(gmem_layout), decltype(smem_layout),
-             decltype(tma_load)>
-      <<<grd_dim, blk_dim>>>(gX, gmem_layout, smem_layout, tma_load);
+  kernel<<<blocks, threads>>>(d_data_ptr, layout, tma_load);
   CUTE_CHECK_LAST();
 
   return 0;
