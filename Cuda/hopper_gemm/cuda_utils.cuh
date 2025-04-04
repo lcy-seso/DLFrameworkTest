75dee1a4262fd311e8b0de43627fdedf766761d6
Ying <lcy.seso@gmail.com>
2025-04-04 19:46:28 +0800

    fix: refine and test tma copy. (#43)
    * Refine and test TMA load.

--- Cuda/hopper_gemm/cuda_utils.cuh
+++ Cuda/hopper_gemm/cuda_utils.cuh
@@ -1,6 +1,8 @@
 #pragma once
 
-namespace utils {
+template <int N, int D>
+constexpr int CeilDiv = (N + D - 1) / D;
+
 float rand_float(float a = 1e-3, float b = 1) {
   float random = ((float)rand()) / (float)RAND_MAX;
   float diff = b - a;
@@ -9,4 +11,3 @@ float rand_float(float a = 1e-3, float b = 1) {
 }
 
 __forceinline__ unsigned int ceil_div(int a, int b) { return (a + b - 1) / b; }
-}  // namespace utils
